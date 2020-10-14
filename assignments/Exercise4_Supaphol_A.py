8d60610d5f5b5fc169d74f19fa1c6f3eb9124e60
Supaphol-Assawalerknun <soupsuki1234@hotmail.com>
2020-10-14 17:17:30 +0700

    Add Exercise 4

---
+++ assignments/Exercise4_Supaphol_A.py
@@ -0,0 +1,9 @@
+FE = 80.0     #Foundation English
+GB = 75.0     #General Business
+ICS = 60.0    #Introduction to Computer Systems
+CP = 90.0     #Computer Programming
+print("-------Your Score--------")
+print("Foundation English: ", FE)
+print("General Business: ", GB)
+print("Introduction to Computer Systems: ", ICS)
+print("Computer Programming: ", CP)
