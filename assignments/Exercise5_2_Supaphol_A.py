d5fca39ad18bc6367233afee25d1421aefd5243a
Supaphol-Assawalerknun <soupsuki1234@hotmail.com>
2020-10-15 16:40:42 +0700

    Added Exercise5_2

---
+++ assignments/Exercise5_2_Supaphol_A.py
@@ -0,0 +1,5 @@
+print("Velocity Calculator")
+distance = int(input("Please enter value of distance (km):"))
+time = int(input("Please enter value of time (h):"))
+velocity = int(distance/time)
+print("Velocity =", velocity, "km/h")
