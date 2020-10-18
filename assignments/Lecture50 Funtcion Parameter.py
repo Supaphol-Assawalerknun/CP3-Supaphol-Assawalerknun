50105a88d8a5953f991ccd14b7a12aad330be2c2
Supaphol-Assawalerknun <soupsuki1234@hotmail.com>
2020-10-18 17:37:43 +0700

    Added Lecture 50

---
+++ assignments/Lecture50 Funtcion Parameter.py
@@ -0,0 +1,16 @@
+def addNumber(x,y):
+    print(x + y)
+
+def removeNumber(x,y):
+    print(x - y)
+
+def multiNumber(x,y):
+    print(x * y)
+
+def DivineNumber(x,y):
+    print(x / y)
+
+addNumber(10,5)
+removeNumber(10,5)
+multiNumber(10,5)
+DivineNumber(10,5)
